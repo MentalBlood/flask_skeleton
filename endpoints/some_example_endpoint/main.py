8ce0a3e656e5c222dbfd1b0fbcce841d4489d62f
mentalblood <neceporenkostepan@gmail.com>
2021-04-29 14:51:13 +0300

    fixed some_example_endpoint name in decorator

--- endpoints/some_example_endpoint/main.py
+++ endpoints/some_example_endpoint/main.py
@@ -1,6 +1,6 @@
 from ..common import *
 
-@routedTo(['/add_message'], 'add_message')
+@routedTo(['/some_example_endpoint'], 'some_example_endpoint')
 class root(Resource):
 	def get(self):
 		return Response(status=200)
